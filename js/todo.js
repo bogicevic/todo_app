c6706d6bceabb4dbd84eb5fc5fdbed4e268398d9
bogicevic <zbogicevic@outlook.com>
2016-04-27 23:05:50 +0200

    Finished styling and functionality

--- js/todo.js
+++ js/todo.js
@@ -22,12 +22,13 @@ $('ul').on('click', 'li', function() {
   $(this).toggleClass('completed');
 });
 
-// click on X to delete todo
+// click on fa-trash to delete todo
 // using on method, for future elements
 $('ul').on('click', 'span', function(event) {
   $(this).parent().fadeOut(300, function() {
     $(this).remove();
   });
+  // prevent event bubbling
   event.stopPropagation();
 })
 
@@ -35,6 +36,10 @@ $('input[type="text"]').keypress(function(event) {
   if(event.which === 13) {
     var todoText = $(this).val();
     $(this).val(' ');
-    $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+    $('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + '</li>');
   }
 })
+
+$('.fa-language').click(function() {
+  $('input[type="text"]').fadeToggle(300);
+});
